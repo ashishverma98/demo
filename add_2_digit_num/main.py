f73e60a86309c0728f4ff70fb4d1c30dd3b18548
ashishverma98 <72750510+ashishverma98@users.noreply.github.com>
2022-06-04 17:45:27 +0530

    Create main.py

---
+++ add_2_digit_num/main.py
@@ -0,0 +1,10 @@
+num = str(input("enter two digit number \n"))
+
+first_digit = int(num[0])
+second_digit = int(num[1])
+
+print(first_digit, second_digit)
+
+sum = first_digit + second_digit
+
+print(sum)
